1202417063957dff213c574780057798ad3e5aaf
jiechic <jiechic@gmail.com>
2017-08-11 17:46:07 +0800

    update release

--- plugin/src/main/groovy/cn/xiaoman/android/router/RouterPlugin.groovy
+++ plugin/src/main/groovy/cn/xiaoman/android/router/RouterPlugin.groovy
@@ -4,7 +4,6 @@ import com.android.build.gradle.AppPlugin
 import com.android.build.gradle.LibraryPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-
 /**
  * Created by zhangqijun on 2017/5/16.
  */
@@ -19,49 +18,50 @@ public class RouterPlugin implements Plugin<Project> {
         def hasApt = project.plugins.hasPlugin('com.neenbedankt.android-apt')
         def hasKotlin = project.plugins.hasPlugin('kotlin-android')
         def hasKapt = project.plugins.hasPlugin('kotlin-kapt')
-        def usesAndroidAspectJxPlugin = project.plugins.hasPlugin('com.archinamon.aspectj') || project.plugins.hasPlugin('android-aspectjx')
 
 
         if (hasApp || hasLib) {
 
-            if (hasApp && !usesAndroidAspectJxPlugin) {
-//                project.pluginManager.apply(com.hujiang.gradle.plugin.android.aspectjx.AndroidAspectJXPlugin);
-                project.pluginManager.apply('android-aspectjx')
-
-//                project.buildscript.repositories.maven { url "https://jitpack.io" }
-//                project.pluginManager.apply('com.archinamon.aspectj')
-            }
             if (hasKotlin && !hasKapt) {
                 project.pluginManager.apply('kotlin-kapt')
             }
 
             project.dependencies {
-                compile 'org.aspectj:aspectjrt:1.8.9'
-                compile 'cn.xiaoman.android.router:router:0.4-SNAPSHOT'
+                compile 'cn.xiaoman.android.router:router:0.5-SNAPSHOT'
 
                 if (hasKotlin) {
-                    kapt 'cn.xiaoman.android.router:compiler:0.4-SNAPSHOT'
+                    kapt 'cn.xiaoman.android.router:compiler:0.5-SNAPSHOT'
                 } else if (hasApt) {
-                    apt 'cn.xiaoman.android.router:compiler:0.4-SNAPSHOT'
+                    apt 'cn.xiaoman.android.router:compiler:0.5-SNAPSHOT'
                 } else {
-                    annotationProcessor 'cn.xiaoman.android.router:compiler:0.4-SNAPSHOT'
+                    annotationProcessor 'cn.xiaoman.android.router:compiler:0.5-SNAPSHOT'
                 }
 
             }
 
-            if (hasKapt) {
-                project.kapt {
-                    arguments {
-                        arg("moduleName", project.getName().replace("-", "_"))
-                    }
-                }
-            } else {
-                project.android.defaultConfig.javaCompileOptions {
-                    annotationProcessorOptions {
-                        arguments = [moduleName: project.getName().replace("-", "_")]
-                    }
-                }
-            }
+//            if (hasApp) {
+//                //拷贝生成的 assets/目录到打包目录
+//                project.afterEvaluate(new Action<Project>() {
+//                    @Override
+//                    void execute(Project project1) {
+//                        project.android.applicationVariants { variant ->
+//                            def variantName = variant.name
+//                            def variantNameCapitalized = variantName.capitalize()
+//                            def copyMetaInf = tasks.create "copyMetaInf$variantNameCapitalized", Copy
+//                            copyMetaInf.from project.fileTree(javaCompile.destinationDir)
+//                            copyMetaInf.include "assets/**"
+//                            copyMetaInf.into "build/intermediates/sourceFolderJavaResources/$variantName"
+//                            tasks.findByName("transformResourcesWithMergeJavaResFor$variantNameCapitalized").dependsOn copyMetaInf
+//                        }
+//                        project.android {
+//                            lintOptions {
+//                                abortOnError false
+//                            }
+//                        }
+//                    }
+//                })
+//
+//            }
 
         }
         project.configurations.all {
